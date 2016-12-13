3aa06ee5398d79abcd4fb357ad1f084a98d8cd68
Robert Fischer <rfischer@axialexchange.com>
2016-12-13 12:27:42 -0500

    Updated dump to improve messaging

--- dump.sh
+++ dump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pg_dump --schema-only --quote-all-identifiers "$USER" > schema.sql && less schema.sql
+pg_dump --verbose --no-owner --schema-only --quote-all-identifiers "$USER" > schema.sql && less schema.sql
